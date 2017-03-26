ec2b48365fd12a0dde6d3b495865579b74fc4346
Sam Whitlock <sam.whitlock@epfl.ch>
2017-03-26 15:35:57 +0200

    adding common dataset encoder for argument parser

--- modules/common/parse.py
+++ modules/common/parse.py
@@ -1,4 +1,11 @@
 import argparse
+import json
+import os
+
+import logging
+logging.basicConfig()
+log = logging.getLogger(__file__)
+log.setLevel(logging.DEBUG)
 
 def numeric_min_checker(minimum, message, numeric_type=int):
     def check_number(n):
@@ -9,3 +16,19 @@ def numeric_min_checker(minimum, message, numeric_type=int):
             ))
         return n
     return check_number
+
+def add_dataset(parser):
+    """
+    Adds the dataset, including parsing, to any parser / subparser
+    """
+    def dataset_parser(filename):
+        if not os.path.isfile(filename):
+            raise argparse.ArgumentTypeError("AGD metadata file not present at {}".format(filename))
+        with open(filename) as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                log.error("Unable to parse AGD metadata file {}".format(filename))
+                raise
+
+    parent_parser.add_argument("dataset", type=dataset_parser, help="The AGD json metadata file describing the dataset")
