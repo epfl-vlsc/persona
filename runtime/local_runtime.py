edce58eac43e211ce3135f3f22820d84c37ca4df
Sam Whitlock <sam.whitlock@epfl.ch>
2017-04-07 15:09:36 +0200

    final fixes for new runtime before test

--- runtime/local_runtime.py
+++ runtime/local_runtime.py
@@ -20,30 +20,31 @@ def execute(args, modules):
   module = modules[args.local]
 
   service = module.get_service()
+  run_arguments = service.extract_run_args(args=args)
+  input_dtypes = service.input_dtypes()
+  input_shapes = service.input_shapes()
 
-  # args ensure every module must specify dataset
-  if os.path.isfile(args.dataset):
-      with open(args.dataset, 'r') as j:
-        dataset = json.load(j)
-      
-      records = dataset['records']
-      chunknames = []
-      for record in records:
-        chunknames.append(record['path'])
+  # We need the batch_join to "close" with a stop exception after enqueuing once
+  # and the FIFOQueue so the graph can decide on its own how much parallelism it wants
+  first_in_queue = tf.train.batch_join(tensors_list=run_arguments, capacity=len(run_arguments),
+                                       batch_size=1)
+  in_queue = tf.FIFOQueue(dtypes=input_dtypes, # [a.dtype for a in first_in_queue],
+                          shapes=input_shapes, # [a.get_shape() for a in first_in_queue],
+                          capacity=len(run_arguments))
+  tf.train.add_queue_runner(
+      tf.train.QueueRunner(enqueue_ops=in_queue.enqueue(first_in_queue))
+  )
 
-      producer = tf.train.string_input_producer(string_tensor=chunknames, num_epochs=1, 
-          shuffle=False, name="executor_input_producer")
-  else:
-      producer = None
-
-  service_ops, service_init_ops = service.make_graph(producer, args)
+  # TODO currently we assume all the service_ops are the same
+  service_ops, service_init_ops = service.make_graph(in_queue=in_queue,
+                                                     args=args)
+  assert len(service_ops) + len(service_init_ops) > 0
 
   init_ops = [tf.global_variables_initializer(), tf.local_variables_initializer()]
-  #init_ops.extend(service_init_ops)
 
   # service graph may have summary nodes
   merged = tf.summary.merge_all()
-  summary = True if hasattr(args, 'summary') else False
+  summary = args.summary if hasattr(args, 'summary') else False
 
   results = []
   with tf.Session() as sess:
@@ -59,19 +60,20 @@ def execute(args, modules):
 
       # its possible the service is a simple run once
       if len(service_ops) > 0:
+          service_sink = tf.train.batch_join(tensors_list=service_ops, batch_size=1)
           coord = tf.train.Coordinator()
           print("Local executor starting {} ...".format(args.local))
           threads = tf.train.start_queue_runners(coord=coord, sess=sess)
           while not coord.should_stop():
               try:
-                  a = sess.run(service_ops)
+                  result = sess.run(service_sink)
                   if summary:
-                      results.append(a[:-1])
-                      summary_writer.add_summary(a[-1], global_step=count)
+                      results.append(result[:-1])
+                      summary_writer.add_summary(result[-1], global_step=count)
                       count += 1
                   else:
-                      results.append(a)
-              except tf.errors.OutOfRangeError as e:
+                      results.append(result)
+              except tf.errors.OutOfRangeError:
                   print('Got out of range error!')
                   break
           print("Coord requesting stop")
