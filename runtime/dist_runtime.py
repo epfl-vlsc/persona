874ee4b7c3dd17e48319dbf19117e6dce15e1ae0
Sam Whitlock <sam.whitlock@epfl.ch>
2017-05-18 16:16:44 +0200

    some work on the global runtime

--- runtime/dist_runtime.py
+++ runtime/dist_runtime.py
@@ -1,8 +1,46 @@
-
 import os
 import tensorflow as tf
 import shutil
+import argparse
+import re
+import time
 from tensorflow.contrib.persona import pipeline
+from modules.common.parse import numeric_min_checker
+
+import logging
+logging.basicConfig()
+log = logging.getLogger(__file__)
+log.setLevel(logging.DEBUG)
+
+cluster_name = "persona"
+startup_wait_time = 1
+host_re = re.compile("(?P<host>(?:\w+)(?:\.\w+)*):(?P<port>\d+)")
+
+def add_cluster_def():
+    def _func(cluster_members):
+        def clusters_transformed():
+            for cluster_member in cluster_members:
+                split = cluster_member.split(",")
+                if len(split) != 2:
+                    raise argparse.ArgumentTypeError("Got badly formed cluster member: '{}'".format(cluster_member))
+                host, port_str = split
+                try:
+                    port = int(port_str)
+                    if host_re.match(host) is None:
+                        raise argparse.ArgumentTypeError("Got invalid host '{h}'. Must be HOST:PORT".format(h=host))
+                    if port < 0:
+                        raise argparse.ArgumentTypeError("Got negative port {p} in member '{m}'".format(p=port, m=cluster_member))
+                    yield host, port
+                except ValueError:
+                    raise argparse.ArgumentTypeError("Unable to convert port '{p}' from member '{m}' into an integer".format(p=port_str, m=cluster_member))
+        cluster = { cluster_name: list(clusters_transformed()) }
+        cluster_spec = tf.train.ClusterSpec(cluster=cluster)
+        return cluster_spec
+    return _func
+
+def add_default_module_args(parser):
+    parser.add_argument("-T", "--task-index", type=numeric_min_checker(minimum=0, message="task index must be non-negative"), required=True, help="TF Cluster task index")
+    parser.add_argument("-C", "--cluster-def", required=True, nargs='+', type=add_cluster_def(), help="TF Cluster definition")
 
 def setup_output_dir(dirname="cluster_traces"):
     trace_path = os.path.join(os.path.dirname(os.getcwd()), dirname)
@@ -27,15 +65,16 @@ def execute(args, modules):
     raise Exception("Service {} does not support distributed execution".format(args.service))
 
   task_index = args.task_index
-  run_arguments = tuple(service.extract_run_args(args=args))
   input_dtypes = service.input_dtypes()
   input_shapes = service.input_shapes()
+  output_dtypes = service.output_dtypes()
+  output_shapes = service.output_shapes()
 
   # we assume that the input queue has name `service`_input and is hosted in the cluster
   # TODO better define the capacity ?
-  in_queue = tf.FIFOQueue(capacity=32, dtypes=input_dtypes, shapes=input_shapes, shared_name=args.service+"_input");
+  in_queue = tf.FIFOQueue(capacity=32, dtypes=input_dtypes, shapes=input_shapes, shared_name=service+"_input")
   # assuming for now that input is the same as output shape and type (generally, string keys for AGD chunks)
-  out_queue = tf.FIFOQueue(capacity=32, dtypes=input_dtypes, shapes=input_shapes, shared_name=args.service+"_output");
+  out_queue = tf.FIFOQueue(capacity=32, dtypes=output_dtypes, shapes=output_shapes, shared_name=service+"_output")
 
   with tf.device("/job:worker/task:"+task_index): # me
 
@@ -79,6 +118,10 @@ def execute(args, modules):
 
       # its possible the service is a simple run once
       if len(service_ops) > 0:
+          uninitialized_vars = tf.report_uninitialized_variables()
+          while len(sess.run(uninitialized_vars)) > 0:
+              log.debug("Waiting for uninitialized variables")
+              time.sleep(1)
           coord = tf.train.Coordinator()
           print("Persona dist executor starting {} ...".format(args.command))
           threads = tf.train.start_queue_runners(coord=coord, sess=sess)
