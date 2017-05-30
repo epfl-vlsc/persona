121c3a32a251b07a89479cce7c4764fc7ff25492
Saket Dingliwal <dingliwa@iccluster129.iccluster.epfl.ch>
2017-05-30 17:51:55 +0200

    arguments added

--- modules/gene_coverage/agd_gene_coverage.py
+++ modules/gene_coverage/agd_gene_coverage.py
@@ -36,7 +36,12 @@ class CalculateCoverageService(Service):
         parser.add_argument("-p", "--parse-parallel", default=1, type=numeric_min_checker(minimum=1, message="read parallelism"),
                             help="total paralellism level for reading data from disk")
 
-        parser.add_argument("-d", "--dataset-dir", type=path_exists_checker(), required=True, help="Directory containing ALL of the chunk files")
+        parser.add_argument("-i", "--dataset-dir", type=path_exists_checker(), required=True, help="Directory containing ALL of the chunk files")
+        parser.add_argument("-scale", "--scale", default=1,type = int, help="change the scale of the coverage found")
+        parser.add_argument("-max", "--max", default=-1, type = int, help="restrict max coverage of histogram")
+        parser.add_argument("-bg", "--bedgraph", default=False, action="store_true", help="output in bedgraph format")
+        parser.add_argument("-d", "--d", default=False, action="store_true", help="reporting per-base genome coverage")
+        parser.add_argument("-strand","--strand", default='B', help="individual coverage for + and - strands")
 
     def make_graph(self, in_queue, args):
         """ Make the graph for this service. Returns two
@@ -52,9 +57,15 @@ class CalculateCoverageService(Service):
         dataset_dir = os.path.dirname(args.dataset_dir)
         op = agd_calculate_coverage_local(in_queue=in_queue,
                                        outdir=dataset_dir,
-                                       parallel_parse=args.parse_parallel)
+                                       parallel_parse=args.parse_parallel,
+                                       scale=args.scale,
+                                       argsj = args,
+                                       max = args.max,
+                                       bg = args.bedgraph,
+                                       d = args.d,
+                                       strand = args.strand)
         run_once = []
-        return op, run_once
+        return [[op]] , run_once
 
 # def _make_writers(compressed_batch, output_dir, write_parallelism):
 #
@@ -84,13 +95,23 @@ def compress_pipeline(results, compress_parallelism):
 
         yield compressed_buf, num_recs, first_ord, record_id
 
-def agd_calculate_coverage_local(in_queue, outdir=None, parallel_parse=1):
+def agd_calculate_coverage_local(in_queue, argsj,outdir=None, parallel_parse=1,scale=1,max=-1,bg=False,d =  False,strand = "B"):
+    manifest = argsj.dataset
+    if 'reference' not in manifest:
+        raise Exception("No reference data in manifest {}. Unaligned BAM not yet supported. Please align dataset first.".format(args.dataset))
+
     """
     key: tensor with chunk key string
     local_directory: the "base path" from which these should be read
     column_grouping_factor: the number of keys to put together
     parallel_parse: the parallelism for processing records (decomp)
     """
+
+    ref_lens = []
+    ref_seqs = []
+    for contig in manifest['reference_contigs']:
+       ref_lens.append(contig['length'])
+       ref_seqs.append(contig['name'])
 
     parallel_key_dequeue = tuple(in_queue.dequeue() for _ in range(parallel_parse))
     result_chunks = pipeline.local_read_pipeline(upstream_tensors=parallel_key_dequeue, columns=['results'])
@@ -113,7 +134,9 @@ def agd_calculate_coverage_local(in_queue, outdir=None, parallel_parse=1):
     print(result_buf)
 
     # bpp = persona_ops.buffer_pair_pool(size=0, bound=False, name="output_buffer_pair_pool")
-    persona_ops.agd_gene_coverage(results_handle=result_buf, num_records=num_results, name="calculatecoverageop")
+
+    result = persona_ops.agd_gene_coverage(results_handle=result_buf, num_records=num_results,ref_sequences=ref_seqs,ref_seq_sizes=ref_lens,scale = scale, max= max,bg = bg,d = d, strand = strand, name="calculatecoverageop")
+
 
     # result_to_write = pipeline.join([result_out, num_results, first_ord, record_id], parallel=parallel_write,
     #     capacity=8, multi=False)
@@ -125,4 +148,4 @@ def agd_calculate_coverage_local(in_queue, outdir=None, parallel_parse=1):
     # recs = list(written)
     # all_written_keys = pipeline.join(recs, parallel=1, capacity=8, multi=False)
     #
-    # return all_written_keys
+    return result
