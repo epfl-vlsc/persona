8158699f63d5df473ac1cf51b408aaa18a727ecb
Stuart Byma <stuart.byma@epfl.ch>
2017-06-14 14:31:07 +0200

    removing data set dir from display

--- modules/display/agd_output.py
+++ modules/display/agd_output.py
@@ -2,6 +2,7 @@
 import tensorflow as tf
 import os
 from ..common.service import Service
+from ..common.parse import *
 
 persona_ops = tf.contrib.persona.persona_ops()
 
@@ -14,7 +15,7 @@ class DisplayService(Service):
     def add_graph_args(self, parser):
         parser.add_argument("start", type=int, help="The absolute index at which to start printing records")
         parser.add_argument("finish", type=int, help="The absolute index at which to stop printing records")
-        parser.add_argument("-d", "--dataset-dir", required=True, help="The directory with the AGD dataset chunk files reside.")
+        parser.add_argument("-d", "--dataset-dir", help="The directory with the AGD dataset chunk files reside.")
         parser.add_argument("-u", "--unpack", default=True, action='store_false', help="Whether or not to unpack binary bases")
 
     def distributed_capability(self):
@@ -28,8 +29,8 @@ class DisplayService(Service):
 
     def extract_run_args(self, args):
         # doesnt really have any effect, since display just runs once 
-        dataset = args.dataset
-        return (a["path"] for a in dataset["records"])
+        
+        return []
 
     def make_graph(self, in_queue, args):
         """ Make the graph for this service. Returns two 
@@ -48,24 +49,19 @@ def run(args):
   first_record = records[0]
   chunk_size = first_record["last"] - first_record["first"]
   chunknames = []
+  dataset_dir = os.path.dirname(args.dataset[filepath_key])
   for record in records:
-    chunknames.append(record['path'])
+    chunknames.append(dataset_dir + "/" + record['path'])
 
   if (args.finish <= args.start):
     args.finish = args.start + 1
 
-  if not os.path.isdir(args.dataset_dir):
-      raise Exception("Dataset directory '{}' doesn't exist".format(args.dataset_dir))
-  pathname = os.path.abspath(args.dataset_dir) + "/"
-
-  path = tf.constant(pathname)
   start = tf.constant(args.start, dtype=tf.int32)
   finish = tf.constant(args.finish, dtype=tf.int32)
   names = tf.constant(chunknames)
   size = tf.constant(chunk_size)
   columns = dataset_params['columns']
-  print(columns)
-  output = persona_ops.agd_output(path=path, chunk_names=names, chunk_size=size, unpack=args.unpack,
+  output = persona_ops.agd_output(chunk_names=names, chunk_size=size, unpack=args.unpack,
       start=start, finish=finish, columns=columns)
 
   return [output]
