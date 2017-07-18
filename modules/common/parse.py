2467153f7f627814d1f212ff9622d3644cc910c1
saketdingliwal <saketdingliwal97@gmail.com>
2017-07-18 17:56:20 +0200

    Added add_multi_dataset

--- modules/common/parse.py
+++ modules/common/parse.py
@@ -25,3 +25,21 @@ def add_dataset(parser):
                 raise
 
     parser.add_argument("dataset", type=dataset_parser, help="The AGD json metadata file describing the dataset")
+
+def add_multi_dataset(parser):
+    """
+    Adds the dataset, including parsing, to any parser / subparser
+    """
+    def dataset_parser(filename):
+        if not os.path.isfile(filename):
+            raise ArgumentTypeError("AGD metadata file not present at {}".format(filename))
+        with open(filename) as f:
+            try:
+                loaded = json.load(f)
+                loaded[filepath_key] = filename
+                return loaded
+            except json.JSONDecodeError:
+                log.error("Unable to parse AGD metadata file {}".format(filename))
+                raise
+
+    parser.add_argument("dataset", nargs= '+',type=dataset_parser, help="The AGD json metadata file describing the dataset")
