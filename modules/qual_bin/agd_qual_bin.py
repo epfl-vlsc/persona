c53d7a321f236fa29d44ad77ff6bee7ae5a561f2
Riya Verma <rverma@iccluster129.iccluster.epfl.ch>
2017-08-02 15:13:57 +0200

    finished qualbin

--- modules/qual_bin/agd_qual_bin.py
+++ modules/qual_bin/agd_qual_bin.py
@@ -33,13 +33,13 @@ class QualBinService(Service):
         return (os.path.join(dataset_dir, a) for a in paths)
 
     def add_graph_args(self, parser):
-        parser.add_argument("-p", "--parse-parallel", default=1, type=numeric_min_checker(minimum=1, message="read parallelism"),
+        parser.add_argument("-p", "--parse-parallel", default=5, type=numeric_min_checker(minimum=1, message="read parallelism"),
                             help="total paralellism level for reading data from disk")
-        parser.add_argument("-w", "--write-parallel", default=1, help="number of writers to use",
+        parser.add_argument("-w", "--write-parallel", default=15, help="number of writers to use",
                             type=numeric_min_checker(minimum=1, message="number of writers min"))
-        parser.add_argument("-pp", "--process-parallel", default=1, help="adjusting parallelism of quality binning stage",type=numeric_min_checker(minimum=1, message="number of parallel processes"))
+        parser.add_argument("-pp", "--process-parallel", default=5, help="adjusting parallelism of quality binning stage",type=numeric_min_checker(minimum=1, message="number of parallel processes"))
         parser.add_argument("-d", "--dataset-dir", type=path_exists_checker(), required=True, help="Directory containing ALL of the chunk files")
-	
+        parser.add_argument("-c", "--parallel-compress", default =30, help="number of compressors", type = numeric_min_checker(minimum=1, message = "number of compressors min"))	
 
     def make_graph(self, in_queue, args):
         """ Make the graph for this service. Returns two 
@@ -56,7 +56,7 @@ class QualBinService(Service):
         op = agd_qual_bin_local(in_queue=in_queue,
                                        outdir=args.dataset_dir, 
                                        parallel_parse=args.parse_parallel, 
-                                       parallel_write=args.write_parallel, parallel_process = args.process_parallel)
+                                       parallel_write=args.write_parallel, parallel_process = args.process_parallel, parallel_compress = args.parallel_compress)
         run_once = []
         return [op], run_once 
 
@@ -67,7 +67,7 @@ def _make_writers(compressed_batch, output_dir, write_parallelism):
     for buf, num_recs, first_ordinal, record_id in compressed_single:
     
         first_ord_as_string = string_ops.as_string(first_ordinal, name="first_ord_as_string")
-        result_key = string_ops.string_join([output_dir, "/", record_id, "_", first_ord_as_string, ".binned_qual"], name="base_key_string")
+        result_key = string_ops.string_join([output_dir, "/", record_id, "_", first_ord_as_string, ".qual"], name="base_key_string")
         
         result = persona_ops.agd_file_system_buffer_writer(record_id=record_id,
                                                      record_type="text",
@@ -88,7 +88,7 @@ def compress_pipeline(results, compress_parallelism):
 
         yield compressed_buf, num_recs, first_ord, record_id
 
-def agd_qual_bin_local(in_queue, outdir=None, parallel_parse=1, parallel_write=1, parallel_compress=1, parallel_process = 2):
+def agd_qual_bin_local(in_queue, outdir=None, parallel_parse=5, parallel_write=15, parallel_compress=30, parallel_process = 5):
     """
     key: tensor with chunk key string
     local_directory: the "base path" from which these should be read
@@ -105,13 +105,13 @@ def agd_qual_bin_local(in_queue, outdir=None, parallel_parse=1, parallel_write=1
     parsed_results = pipeline.agd_reader_multi_column_pipeline(upstream_tensorz=result_chunk_list)
     parsed_results_list = list(parsed_results)
 
-    parsed_result = pipeline.join(parsed_results_list, parallel=parallel_process, capacity=8, multi=True)
+    parsed_result = pipeline.join(parsed_results_list, parallel=parallel_process, capacity=8, multi=True, name = "name_for_debugging")
 
     # result_buf, num_recs, first_ord, record_id
     #parsed_results = tf.contrib.persona.persona_in_pipe(key=key, dataset_dir=local_directory, columns=["qual_test"], parse_parallel=parallel_parse,
                                                         #process_parallel=1)
   
-    print(parsed_result)
+    #print(parsed_result)
     #result_buf, num_results, first_ord, record_id = parsed_result
     #result_buf = tf.unstack(result_buf)[0]
     #print(result_buf)
@@ -121,12 +121,13 @@ def agd_qual_bin_local(in_queue, outdir=None, parallel_parse=1, parallel_write=1
     output_list = []
     for entry in parsed_result:
     	result_buf, num_results, first_ord, record_id = entry
-    	print(result_buf)
+    	#print(result_buf)
     	result_buf = tf.unstack(result_buf)[0] 
     	result_out = persona_ops.agd_qual_bin(results_handle=result_buf, num_records=num_results,buffer_pair_pool=bpp, name="qualbinop", upper_bounds = [9, 19, 24, 29, 34, 39,40], bin_values = [6, 15, 22, 27, 33, 37, 40], encoding_offset = 33)
     	output_list.append([result_out, num_results, first_ord, record_id])
-
-    result_to_write = pipeline.join(output_list, parallel=parallel_write, 
+    print("output_list")
+    print(output_list)
+    result_to_write = pipeline.join(output_list, parallel=parallel_compress, 
         capacity=8, multi=True)
 #changed line about
 
