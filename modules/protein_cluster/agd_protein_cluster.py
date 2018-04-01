7633b666ddcbba8d85d07d5e2ee5f64759d85e13
Stuart Byma <stuart.byma@epfl.ch>
2018-04-01 12:12:51 +0200

    fixes to get things working

--- modules/protein_cluster/agd_protein_cluster.py
+++ modules/protein_cluster/agd_protein_cluster.py
@@ -48,7 +48,7 @@ class ProteinClusterService(Service):
                 raise ArgumentTypeError("Datasets do not have equal chunk sizes")
             for p in t[1]["records"]:
                 paths.append(os.path.join(t[0], p["path"]))
-                self.total_chunks++
+                self.total_chunks += 1
 
         return paths
 
@@ -68,21 +68,140 @@ class ProteinClusterService(Service):
                             type=numeric_min_checker(minimum=1, message="number of writers min"))
         parser.add_argument("-n", "--nodes", default=1, help="number of ring nodes",
                             type=numeric_min_checker(minimum=1, message="number of nodes min"))
+        
+        parser.add_argument("-c", "--config", default="./params.json", help="JSON config file")
 
     def make_graph(self, in_queue, args):
         """ Make the graph for this service. Returns two 
         things: a list of tensors which the runtime will 
         evaluate, and a list of run-once ops"""
         # make the graph
+        args.config = json.load(open(args.config))
 
         if in_queue is None:
             raise EnvironmentError("in queue is none")
 
-        ops = agd_protein_cluster_local(in_queue=in_queue,
-                                       parallel_parse=args.parse_parallel, 
-                                       parallel_write=args.write_parallel)
+        ops = self.agd_protein_cluster_local(in_queue, args)
         run_once = []
+        print(ops)
+        print(os.getpid())
+        import ipdb; ipdb.set_trace()
         return ops, run_once 
+
+    def make_ring(self, args, num_nodes, input_queue, envs, shapes, types):
+
+        left_queue = None
+        ops = []
+        cluster_tensors_out = []
+
+        prev_nb_queue = None
+
+        for i in range(num_nodes):
+            op, nb, nbo, cto = self.make_ring_node(args, input_queue, envs, i, shapes, types)
+            ops.append(op)
+            cluster_tensors_out.append(cto)
+            if i == 0:
+                left_queue = nb
+            if prev_nb_queue:
+                enq = nb.enqueue(prev_nb_queue.dequeue())
+                tf.train.queue_runner.add_queue_runner(tf.train.queue_runner.QueueRunner(nb, [enq]))
+
+            prev_nb_queue = nbo
+
+            if i == num_nodes - 1:
+                print("adding last neighbor queue runner")
+                enq = left_queue.enqueue(nbo.dequeue())
+                tf.train.queue_runner.add_queue_runner(tf.train.queue_runner.QueueRunner(left_queue, [enq]))
+
+        cluster_tensor_out = pipeline.join([ [x] for x in cluster_tensors_out], parallel=1, capacity=32, multi=True)
+
+        #ops.append(cluster_tensor_out)
+        return [ops, cluster_tensor_out]
+
+    def make_ring_node(self, args, input_queue, envs, node_id, shapes, types):
+        # output (op, neighbor_queue, neighbor_queue_out, cluster_tensor_out)
+        nb_q = data_flow_ops.FIFOQueue(capacity=2,  # TODO capacity
+                dtypes=types,
+                shapes=shapes, name="nb_"+str(node_id))
+
+        nb_q_o = data_flow_ops.FIFOQueue(capacity=2,  # TODO capacity
+                dtypes=types,
+                shapes=shapes, name="nbo_"+str(node_id))
+        
+        c_q = data_flow_ops.FIFOQueue(capacity=2,  # TODO capacity
+                dtypes=[tf.string], shapes=[tf.TensorShape([])])
+
+        cluster_tensor_out = c_q.dequeue()
+        
+        op = persona_ops.agd_protein_cluster(input_queue=input_queue.queue_ref, neighbor_queue=nb_q.queue_ref, neighbor_queue_out=nb_q_o.queue_ref, 
+                cluster_queue=c_q.queue_ref, alignment_envs=envs, node_id=node_id, ring_size=args.nodes, min_score=args.config["min_score"],
+                max_reps=args.config["max_reps"], max_n_aa_not_covered=args.config["max_n_aa_not_covered"], total_chunks=self.total_chunks, name="protclustop")
+
+        return (op, nb_q, nb_q_o, cluster_tensor_out)
+
+
+    def agd_protein_cluster_local(self, in_queue, args, parallel_parse=1, parallel_write=1, parallel_compress=1):
+        """
+        key: tensor with chunk key string
+        local_directory: the "base path" from which these should be read
+        column_grouping_factor: the number of keys to put together
+        parallel_parse: the parallelism for processing records (decomp)
+        """
+      
+        parallel_key_dequeue = tuple(in_queue.dequeue() for _ in range(parallel_parse))
+        result_chunks = pipeline.local_read_pipeline(upstream_tensors=parallel_key_dequeue, columns=['prot'])
+
+        result_chunk_list = [ list(c) for c in result_chunks ]
+
+        
+        parsed_results = pipeline.agd_reader_multi_column_pipeline(upstream_tensorz=result_chunk_list)
+        parsed_results_list = list(parsed_results)
+
+        parsed_result = pipeline.join(parsed_results_list, parallel=1, capacity=8, multi=True)[0]
+
+        # result_buf, num_recs, first_ord, record_id
+        #parsed_results = tf.contrib.persona.persona_in_pipe(key=key, dataset_dir=local_directory, columns=["results"], parse_parallel=parallel_parse,
+                                                            #process_parallel=1)
+      
+        print(parsed_result)
+        result_buf, num_recs, first_ord, record_id = parsed_result
+        result_buf = tf.unstack(result_buf)[0]
+        print(result_buf)
+
+        protein_tensor = persona_ops.agd_chunk_to_tensor(result_buf)
+        # form main input queue with protein_tensor, num_recs, sequence, was_added, coverages
+        shapes = [protein_tensor.shape, num_recs.shape, tf.TensorShape([]), tf.TensorShape([self.chunk_size]), tf.TensorShape([self.chunk_size])]
+        types = [protein_tensor.dtype.base_dtype, num_recs.dtype, tf.int32, tf.bool, tf.string]
+        q = data_flow_ops.FIFOQueue(capacity=2,  # TODO capacity
+                dtypes=types,
+                shapes=shapes)
+
+        print(shapes)
+        seq = tf.constant(0)
+        was_added = tf.constant([ False for x in range(self.chunk_size)])
+        coverages = tf.constant([ "" for x in range(self.chunk_size)])
+        enq = q.enqueue([protein_tensor, num_recs, seq, was_added, coverages])
+            
+        tf.train.queue_runner.add_queue_runner(tf.train.queue_runner.QueueRunner(q, [enq]))
+
+        envs = environments.make_envs_op() 
+
+        all_ops = self.make_ring(args, args.nodes, q, envs, shapes, types)
+
+        #result_to_write = pipeline.join([result_out, num_results, first_ord, record_id], parallel=parallel_write, 
+            #capacity=8, multi=False)
+
+        #compressed = compress_pipeline(result_to_write, parallel_compress)
+
+        #written = _make_writers(compressed_batch=list(compressed), output_dir=outdir, write_parallelism=parallel_write)
+
+        #recs = list(written)
+        #all_written_keys = pipeline.join(recs, parallel=1, capacity=8, multi=False)
+
+        return all_ops
+
+
+
 
 def _make_writers(compressed_batch, output_dir, write_parallelism):
 
@@ -112,108 +231,5 @@ def compress_pipeline(results, compress_parallelism):
 
         yield compressed_buf, num_recs, first_ord, record_id
 
-def make_ring(num_nodes, input_queue, envs):
-
-    left_queue = None
-    ops = []
-    cluster_tensors_out = []
-
-    prev_nb_queue = None
-
-    for i in range(num_nodes):
-        op, nb, nbo, cto = make_ring_node(input_queue, envs, i)
-        ops.append(op)
-        cluster_tensors_out.append(cto)
-        if i == 0:
-            left_queue = nb
-        if prev_nb_queue:
-            nb.enqueue(prev_nb_queue.dequeue())
-        prev_nb_queue = nbo
-
-        if i == num_nodes - 1:
-            left_queue.enqueue(nbo.dequeue())
-
-    cluster_tensor_out = pipeline.join([ x.dequeue() for x in cluster_tensors_out], parallel=1, capacity=32, multi=True)[0]
-
-    return (ops, cluster_tensor_out)
-
-def make_ring_node(input_queue, envs, node_id):
-    # output (op, neighbor_queue, neighbor_queue_out, cluster_tensor_out)
-    nb_q = data_flow_ops.FIFOQueue(capacity=2,  # TODO capacity
-            dtypes=[protein_tensor.dtype.base_dtype, num_recs.dtype, tf.int32, tf.bool, tf.string],
-            shapes=[protein_tensor.shape, num_recs.shape, tf.TensorShape([]), tf.TensorShape([self.chunk_size]), tf.TensorShape([self.chunk_size])])
-
-    nb_q_o = data_flow_ops.FIFOQueue(capacity=2,  # TODO capacity
-            dtypes=[protein_tensor.dtype.base_dtype, num_recs.dtype, tf.int32, tf.bool, tf.string],
-            shapes=[protein_tensor.shape, num_recs.shape, tf.TensorShape([]), tf.TensorShape([self.chunk_size]), tf.TensorShape([self.chunk_size])])
-    
-    c_q = data_flow_ops.FIFOQueue(capacity=2,  # TODO capacity
-            dtypes=[tf.string], shapes=[tf.TensorShape([])])
-
-    cluster_tensor_out = c_q.dequeue()
-    
-    op = persona_ops.agd_protein_cluster(input_queue=input_queue.queue_ref, neighbor_queue=nb_q.queue_ref, neighbor_queue_out=nb_q.queue_ref, 
-            cluster_queue=c_q.queue_ref, alignment_envs=envs, node_id=node_id, name="protclustop")
-
-    return (op, nb_q, nb_q_o, cluster_tensor_out)
-
-def make_envs():
-
-def agd_protein_cluster_local(in_queue, parallel_parse=1, parallel_write=1, parallel_compress=1):
-    """
-    key: tensor with chunk key string
-    local_directory: the "base path" from which these should be read
-    column_grouping_factor: the number of keys to put together
-    parallel_parse: the parallelism for processing records (decomp)
-    """
-  
-    parallel_key_dequeue = tuple(in_queue.dequeue() for _ in range(parallel_parse))
-    result_chunks = pipeline.local_read_pipeline(upstream_tensors=parallel_key_dequeue, columns=['prot'])
-
-    result_chunk_list = [ list(c) for c in result_chunks ]
-
-    
-    parsed_results = pipeline.agd_reader_multi_column_pipeline(upstream_tensorz=result_chunk_list)
-    parsed_results_list = list(parsed_results)
-
-    parsed_result = pipeline.join(parsed_results_list, parallel=1, capacity=8, multi=True)[0]
-
-    # result_buf, num_recs, first_ord, record_id
-    #parsed_results = tf.contrib.persona.persona_in_pipe(key=key, dataset_dir=local_directory, columns=["results"], parse_parallel=parallel_parse,
-                                                        #process_parallel=1)
-  
-    print(parsed_result)
-    result_buf, num_results, first_ord, record_id = parsed_result
-    result_buf = tf.unstack(result_buf)[0]
-    print(result_buf)
-
-    protein_tensor = persona_ops.agd_chunk_to_tensor(result_buf)
-    # form main input queue with protein_tensor, num_recs, sequence, was_added, coverages
-    q = data_flow_ops.FIFOQueue(capacity=2,  # TODO capacity
-            dtypes=[protein_tensor.dtype.base_dtype, num_recs.dtype, tf.int32, tf.bool, tf.string],
-            shapes=[protein_tensor.shape, num_recs.shape, tf.TensorShape([]), tf.TensorShape([self.chunk_size]), tf.TensorShape([self.chunk_size])])
-
-    seq = tf.constant([ 0 for x in range(self.chunk_size)])
-    was_added = tf.constant([ False for x in range(self.chunk_size)])
-    coverages = tf.constant([ False for x in range(self.chunk_size)])
-    enq = q.enqueue([protein_tensor, num_results, seq, was_added, coverages])
-        
-    tf.train.queue_runner.add_queue_runner(tf.train.queue_runner.QueueRunner(q, [enq]))
-
-    envs = make_envs() # whatever stuff goes in here
-
-    all_ops = make_ring(num_nodes, q, envs)
-
-    #result_to_write = pipeline.join([result_out, num_results, first_ord, record_id], parallel=parallel_write, 
-        #capacity=8, multi=False)
-
-    #compressed = compress_pipeline(result_to_write, parallel_compress)
-
-    #written = _make_writers(compressed_batch=list(compressed), output_dir=outdir, write_parallelism=parallel_write)
-
-    #recs = list(written)
-    #all_written_keys = pipeline.join(recs, parallel=1, capacity=8, multi=False)
-
-    return all_written_keys
   
   
