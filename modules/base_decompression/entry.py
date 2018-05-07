83456079d3c07690297ddcd9f1bdeb80dbc584fa
Arnaud Didier Marie Boissaye <boissaye@iccluster129.iccluster.epfl.ch>
2018-05-07 15:58:25 +0200

    decompression op work in progress

--- modules/base_decompression/entry.py
+++ modules/base_decompression/entry.py
@@ -1,10 +1,10 @@
 from . import agd_base_decompression
 from ..common import service
 
-class DisplaySingleton(service.ServiceSingleton):
-  class_type = agd_base_decompression.DisplayService
+class BaseDecompressionSingleton(service.ServiceSingleton):
+  class_type = agd_base_decompression.AGDBaseDecompressionService
 
-_singletons = [ DisplaySingleton() ]
+_singletons = [ BaseDecompressionSingleton() ]
 _service_map = { a.get_shortname(): a for a in _singletons }
 
 def get_tooltip():
@@ -15,4 +15,3 @@ def get_services():
 
 def lookup_service(name):
   return _service_map[name]
-
